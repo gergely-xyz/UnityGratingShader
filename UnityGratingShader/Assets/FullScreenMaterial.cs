6d0bce74320a0d7ebd657ffee749ebfc75de9bff
gergely-xyz <horvath@gergely.xyz>
2019-03-31 23:29:34 +0200

    Change name inside the script too

--- UnityGratingShader/Assets/FullScreenMaterial.cs
+++ UnityGratingShader/Assets/FullScreenMaterial.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class FullScreenGrating : MonoBehaviour {
+public class FullScreenMaterial : MonoBehaviour {
     public Material material;
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
