b1d97373c2dd98785247ec3fa1071b9e4fd3f3b1
gergely-xyz <horvath@gergely.xyz>
2019-04-11 11:42:27 +0200

    Random clouds example added

---
+++ UnityGratingShader/Assets/RandomizeCloud.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeCloud : MonoBehaviour {
+    private Renderer r;
+
+    void Start () {
+        r = GetComponent<Renderer>();
+        r.materials[0].SetFloat("_offsetX", Random.Range(-1000f, 1000f));
+        r.materials[0].SetFloat("_offsetY", 3f);
+    }
+
+}
