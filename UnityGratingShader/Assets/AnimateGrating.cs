3d8f97aa22e64f0763686569d3a1541975598a49
gergely-xyz <horvath@gergely.xyz>
2019-04-11 10:58:05 +0200

    Manual animation with script

---
+++ UnityGratingShader/Assets/AnimateGrating.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateGrating : MonoBehaviour {
+    private Renderer r;
+    private float state;
+    public float speed = 1;
+    public bool paused;
+
+    void Start () {
+        r = GetComponent<Renderer>();
+        r.materials[0].SetFloat("_Speed", 0f);
+    }
+	
+	void FixedUpdate () {
+        if (!paused) {
+            r.materials[0].SetFloat("_Offset", state);
+            state += speed;
+            state %= 360;
+        }
+        
+    }
+}
